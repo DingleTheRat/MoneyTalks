652598693546df5856a9dbc051e2556c6bb2c855
Dingle <Stepa202120212021@gmail.com>
2026-04-13 20:00:39 -0600

    Merchant's Carpets drop themselfs

--- src/client/java/net/dingletherat/datagen/MoneyLootTableProvider.java
+++ src/client/java/net/dingletherat/datagen/MoneyLootTableProvider.java
@@ -17,5 +17,22 @@ public class MoneyLootTableProvider extends FabricBlockLootSubProvider {
     public void generate() {
         dropSelf((Block) MoneyBlocks.TRIMMED_HOPPER);
         dropSelf((Block) MoneyBlocks.DOUBLE_TRIMMED_HOPPER);
+
+        dropSelf((Block) MoneyBlocks.RED_MERCHANT_CARPET);
+        dropSelf((Block) MoneyBlocks.ORANGE_MERCHANT_CARPET);
+        dropSelf((Block) MoneyBlocks.YELLOW_MERCHANT_CARPET);
+        dropSelf((Block) MoneyBlocks.LIME_MERCHANT_CARPET);
+        dropSelf((Block) MoneyBlocks.GREEN_MERCHANT_CARPET);
+        dropSelf((Block) MoneyBlocks.CYAN_MERCHANT_CARPET);
+        dropSelf((Block) MoneyBlocks.LIGHT_BLUE_MERCHANT_CARPET);
+        dropSelf((Block) MoneyBlocks.BLUE_MERCHANT_CARPET);
+        dropSelf((Block) MoneyBlocks.PURPLE_MERCHANT_CARPET);
+        dropSelf((Block) MoneyBlocks.MAGENTA_MERCHANT_CARPET);
+        dropSelf((Block) MoneyBlocks.PINK_MERCHANT_CARPET);
+        dropSelf((Block) MoneyBlocks.WHITE_MERCHANT_CARPET);
+        dropSelf((Block) MoneyBlocks.LIGHT_GRAY_MERCHANT_CARPET);
+        dropSelf((Block) MoneyBlocks.GRAY_MERCHANT_CARPET);
+        dropSelf((Block) MoneyBlocks.BLACK_MERCHANT_CARPET);
+        dropSelf((Block) MoneyBlocks.BROWN_MERCHANT_CARPET);
     }
 }
