9e5521658d31d5a0c00871c5f31a2851a52e5599
Dingle <Stepa202120212021@gmail.com>
2026-05-09 22:24:08 -0600

    Fixed textures and datagen

--- src/main/java/net/dingletherat/datagen/MoneyModels.java
+++ src/main/java/net/dingletherat/datagen/MoneyModels.java
@@ -6,15 +6,24 @@ import net.dingletherat.item.MoneyItems;
 import net.minecraft.client.data.models.BlockModelGenerators;
 import net.minecraft.client.data.models.ItemModelGenerators;
 import net.minecraft.client.data.models.ModelProvider;
+import net.minecraft.client.data.models.MultiVariant;
+import net.minecraft.client.data.models.blockstates.MultiVariantGenerator;
+import net.minecraft.client.data.models.blockstates.PropertyDispatch;
 import net.minecraft.client.data.models.model.ModelTemplate;
 import net.minecraft.client.data.models.model.ModelTemplates;
 import net.minecraft.client.data.models.model.TextureMapping;
 import net.minecraft.client.data.models.model.TextureSlot;
+import net.minecraft.client.renderer.block.dispatch.Variant;
+import net.minecraft.core.Direction;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.Identifier;
+import net.minecraft.util.random.WeightedList;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 
 import java.util.Optional;
+
+import com.mojang.math.Quadrant;
 
 public class MoneyModels extends ModelProvider {
     public MoneyModels(PackOutput output) {
@@ -57,5 +66,25 @@ public class MoneyModels extends ModelProvider {
 
         itemModels.generateFlatItem(MoneyItems.DOLLAR.get(), ModelTemplates.FLAT_ITEM);
         itemModels.generateFlatItem(MoneyItems.WALLET.get(), ModelTemplates.FLAT_ITEM);
+
+        registerHopper(blockModels, MoneyBlocks.TRIMMED_HOPPER.get(), "trimmed_hopper");
+        registerHopper(blockModels, MoneyBlocks.DOUBLE_TRIMMED_HOPPER.get(), "double_trimmed_hopper");
+    }
+
+    private void registerHopper(BlockModelGenerators gen, Block block, String name) {
+        Identifier down = Identifier.fromNamespaceAndPath(MoneyTalks.MOD_ID, "block/" + name);
+        Identifier side = Identifier.fromNamespaceAndPath(MoneyTalks.MOD_ID, "block/" + name + "_side");
+
+        gen.blockStateOutput.accept(
+            MultiVariantGenerator.dispatch(block)
+                .with(PropertyDispatch.initial(BlockStateProperties.FACING)
+                    .select(Direction.DOWN,  new MultiVariant(WeightedList.of(new Variant(down))))
+                    .select(Direction.NORTH, new MultiVariant(WeightedList.of(new Variant(side))))
+                    .select(Direction.EAST,  new MultiVariant(WeightedList.of(new Variant(side).withYRot(Quadrant.R90))))
+                    .select(Direction.SOUTH, new MultiVariant(WeightedList.of(new Variant(side).withYRot(Quadrant.R180))))
+                    .select(Direction.WEST,  new MultiVariant(WeightedList.of(new Variant(side).withYRot(Quadrant.R270))))
+                    .select(Direction.UP,    new MultiVariant(WeightedList.of(new Variant(down))))
+                )
+        );
     }
 }
